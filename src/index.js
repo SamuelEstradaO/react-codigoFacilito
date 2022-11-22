d78433f4824c3bff9d5aada7c748c2b17aae05ff
SamuelEstradaO <87049068+SamuelEstradaO@users.noreply.github.com>
2022-11-21 23:23:22 -0600

    class state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludar = ({ nombre, idioma = 'en' }) => {
-  const saludo = idioma === 'es' ? 'Hola' : 'Hello';
-  return (
-    <p>
-      {saludo} {nombre}
-    </p>
-  );
-};
+// const Saludar = ({ nombre, idioma = 'en' }) => {
+//   const saludo = idioma === 'es' ? 'Hola' : 'Hello';
+//   return (
+//     <p>
+//       {saludo} {nombre}
+//     </p>
+//   );
+// };
+
+const Button = ( ) => {
+  const [counter, setCounter] = useState(0);
+  return (<div>
+      <p>Presionado: {counter}</p>
+      <button onClick={()=>{setCounter(counter+1)}}>Click me!</button>
+  </div>)
+}
 
 // Saludar.defaultProps ={
 //   idioma: 'en'
@@ -17,7 +25,7 @@ const Saludar = ({ nombre, idioma = 'en' }) => {
 const App = () => {
   return (
     <div>
-      <Saludar nombre="Samuel"/>
+      <Button />
     </div>
   );
 };
