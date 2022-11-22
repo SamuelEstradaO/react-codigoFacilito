4154add214768f00a9aaafddeb0605cade6d5366
SamuelEstradaO <87049068+SamuelEstradaO@users.noreply.github.com>
2022-11-22 15:37:56 -0600

    class sendForms

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludo = ({ nombre, idioma = 'en' }) => {
-  const [name, setName] = useState('');
-  return (
-    <div>
-      <input type="text" onChange={(ev) => setName(ev.target.value)} />
-      <p>Hola {name}</p>
-    </div>
-  );
-};
+// const Saludo = ({ nombre, idioma = 'en' }) => {
+//   const [name, setName] = useState('');
+//   return (
+//     <div>
+//       <input type="text" onChange={(ev) => setName(ev.target.value)} />
+//       <p>Hola {name}</p>
+//     </div>
+//   );
+// };
 
 // const Button = () => {
 //   const [counter, setCounter] = useState(0);
@@ -36,11 +36,57 @@ const Saludo = ({ nombre, idioma = 'en' }) => {
 //   idioma: 'en'
 // }
 
+const Form = () => {
+  let [title, setTitle] = useState('');
+  let [body, setBody] = useState('');
+  const sendForm = (ev) => {
+    ev.preventDefault();
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setTitle('');
+        setBody('');
+        console.log(json);
+      });
+  };
+  return (
+    <form onSubmit={(ev) => sendForm(ev)}>
+      <div>
+        <label htmlFor="title">Título</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="body">Publicación</label>
+        <textarea
+          id="body"
+          value={body}
+          onChange={(ev) => setBody(ev.target.value)}
+        ></textarea>
+      </div>
+      <input type="submit" value="Enviar" />
+    </form>
+  );
+};
+
 const App = () => {
-  const [showButton, setShowButton] = useState(true);
   return (
     <div>
-      <Saludo />
+      <Form />
     </div>
   );
 };
